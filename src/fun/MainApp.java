1553f027fa5442aacfba5aa60fea38b2c0e414b0
zach <zachcelona@gmail.com>
2023-09-17 02:42:27 +1200

    some minor changes

--- src/fun/MainApp.java
+++ src/fun/MainApp.java
@@ -19,21 +19,13 @@ public class MainApp extends Application {
     Parent root = FXMLLoader.load(url);
 
     Scene scene = new Scene(root);
-    scene.getStylesheets().add("/styles/Styles.css");
 
-    stage.setTitle("JavaFX and Maven");
+    stage.setTitle("Minesweeper");
     stage.setScene(scene);
     stage.show();
   }
 
-  /**
-   * The main() method is ignored in correctly deployed JavaFX application. main() serves only as
-   * fallback in case the application can not be launched through deployment artifacts, e.g., in
-   * IDEs with limited FX support. NetBeans ignores main().
-   *
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    launch(args);
+  public static void main() {
+    launch();
   }
 }
