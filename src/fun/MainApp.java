df04ea49603d57db3ffb14ae0b3ce0f4dbf25610
zach <zachcelona@gmail.com>
2023-09-17 02:38:21 +1200

    first commit

---
+++ src/fun/MainApp.java
@@ -0,0 +1,39 @@
+package fun;
+
+import java.io.File;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    URL url =
+        new File("C:\\Users\\Zach\\Desktop\\Minesweeper\\src\\fun\\resources\\fxml\\Scene.fxml")
+            .toURI()
+            .toURL();
+    Parent root = FXMLLoader.load(url);
+
+    Scene scene = new Scene(root);
+    scene.getStylesheets().add("/styles/Styles.css");
+
+    stage.setTitle("JavaFX and Maven");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * The main() method is ignored in correctly deployed JavaFX application. main() serves only as
+   * fallback in case the application can not be launched through deployment artifacts, e.g., in
+   * IDEs with limited FX support. NetBeans ignores main().
+   *
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
